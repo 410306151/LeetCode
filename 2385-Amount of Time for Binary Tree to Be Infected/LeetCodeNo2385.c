054fb7c6ce150095b1b5268565da9293b814cd44
Hapache <94890792+Hapache@users.noreply.github.com>
2022-08-30 17:14:38 +0800

    更改變數名稱

--- 2385-Amount of Time for Binary Tree to Be Infected/LeetCodeNo2385.c
+++ 2385-Amount of Time for Binary Tree to Be Infected/LeetCodeNo2385.c
@@ -23,7 +23,7 @@ int countLayer(struct TreeNode* root){
 	return fmax(left, right) + 1;
 }
 
-int infect(struct TreeNode* root, int start, int *returnCount, int *layer){
+int infect(struct TreeNode* root, int start, int *ans, int *nodes){
 	int left, right;
 
 	if(root == NULL){
@@ -31,21 +31,21 @@ int infect(struct TreeNode* root, int start, int *returnCount, int *layer){
 	}
 	if(root->val == start){
 		// Found the target, checking which side has deeper layer
-		*returnCount = countLayer(root) - 1;
+		*ans = countLayer(root) - 1;
 		return -1;
 	}
 	if(root->left == NULL && root->right == NULL){
 		return 1;
 	}
 
-	left = infect(root->left, start, returnCount, layer);
-	right = infect(root->right, start, returnCount, layer);
+	left = infect(root->left, start, ans, nodes);
+	right = infect(root->right, start, ans, nodes);
 	if(left == -1){
 		// The target is in left side of current node, checking layers of right node
 		// Return -1 becuase found the target from current node
 		// *layer + 1 becuase we need to trace back how many nodes we need to infect to current node
-		*layer += 1;
-		*returnCount = fmax(*returnCount, right + *layer);
+		*nodes += 1;
+		*ans = fmax(*ans, right + *nodes);
 
 		return -1;
 	}
@@ -53,8 +53,8 @@ int infect(struct TreeNode* root, int start, int *returnCount, int *layer){
 		// The target is in right side of current node, checking layers of left node
 		// Return -1 becuase found the target from current node
 		// *layer + 1 becuase we need to trace back how many nodes we need to infect to current node
-		*layer += 1;
-		*returnCount = fmax(*returnCount, left + *layer);
+		*nodes += 1;
+		*ans = fmax(*ans, left + *nodes);
 
 		return -1;
 	}
@@ -63,7 +63,7 @@ int infect(struct TreeNode* root, int start, int *returnCount, int *layer){
 }
 
 int amountOfTime(struct TreeNode* root, int start){
-	int ans = 0, layer = 0;
+	int ans = 0, nodes = 0;
 
 	if(root == NULL){
 		return 0;
@@ -74,7 +74,7 @@ int amountOfTime(struct TreeNode* root, int start){
 	if(root->val == start){
 		return countLayer(root) - 1;
 	}
-	infect(root, start, &ans, &layer);
+	infect(root, start, &ans, &nodes);
 
 	return ans;
 }
