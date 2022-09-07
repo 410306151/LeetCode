be0f12f56431ea3ee2c6b7cf1fd19ae83708c7b8
Hapache <94890792+Hapache@users.noreply.github.com>
2022-09-07 15:39:09 +0800

    原本時間複雜度是O(n)，改成O(logN)的方法

--- 33-Search in Rotated Sorted Array/LeetCodeNo33.c
+++ 33-Search in Rotated Sorted Array/LeetCodeNo33.c
@@ -3,46 +3,43 @@
 #include "string.h"
 
 int binarySearch(int nums[], int target, int left, int right){
-	int mid = (left + right) / 2;
-
-	if(right == left && nums[left] != target){
-		return -1;
-	} else if(nums[mid] == target){
-		return mid;
-	}
-
-	if(nums[mid] > target){
-		// Left array
-		return binarySearch(nums, target, left, mid);
-	} else{
-		// Right array
-		return binarySearch(nums, target, mid + 1, right);
-	}
+    int mid = (left + right) / 2;
+    
+    if(right == left && nums[left] != target){
+        return -1;
+    }else if(nums[mid] == target){
+        return mid;
+    }
+    
+    if(target < nums[mid] && target > nums[right]){
+        // Rotated array, target at left array
+        return binarySearch(nums, target, left, mid);
+    }else if(target > nums[mid] && target < nums[left]){
+        // Rotated array, target at right array
+        return binarySearch(nums, target, mid + 1, right);
+    }else if(target < nums[left] || target > nums[right]){
+        // Rotated array, target can be left or right array
+        if(nums[mid] < nums[right]){
+            // Left array
+            return binarySearch(nums, target, left, mid);
+        }else{
+            // Right array
+            return binarySearch(nums, target, mid + 1, right);
+        }
+    }else{
+        // Regular binary search
+        if(nums[mid] > target){
+            // Left array
+            return binarySearch(nums, target, left, mid);
+        }else{
+            // Right array
+            return binarySearch(nums, target, mid + 1, right);
+        }
+    }
+    
 }
 
 int search(int* nums, int numsSize, int target){
-	int rotatePos = -1;
-
-	for(int i = 1; i < numsSize; i++){
-		if(nums[i] < nums[i - 1]){
-			rotatePos = i - 1;
-			break;
-		}
-	}
-	// Rotated array
-	if(rotatePos != -1){
-		if(target > nums[rotatePos]){
-			// Target > maximum value of array
-			return -1;
-		} else if(target < nums[rotatePos + 1]){
-			// Target < minimum value of array
-			return -1;
-		} else if(target >= nums[0]){
-			return binarySearch(nums, target, 0, rotatePos);
-		} else if(target < nums[0]){
-			return binarySearch(nums, target, rotatePos + 1, numsSize - 1);
-		}
-	}
-	// Array not rotate
-	return binarySearch(nums, target, 0, numsSize - 1);
+    
+    return binarySearch(nums, target, 0, numsSize - 1);
 }
